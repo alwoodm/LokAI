ec17a08b2483a04ed3c524209e9a185a3e3dd1d9
alwoodm <oleklisalwood@proton.me>
2025-03-24 21:40:09 +0100

    Aktualizacja wersji Android NDK
    Zaktualizowano wersję Android NDK do 27.0.12077973 wymaganej przez plugin path_provider_android. Ta aktualizacja rozwiązuje ostrzeżenia kompilacji i zapewnia kompatybilność ze wszystkimi używanymi pluginami.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.lokai.lokai"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"  // Dodana linia z wymaganą wersją NDK
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
